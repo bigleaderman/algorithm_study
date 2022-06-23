3366434a9dcec5007ef21d10fbcd70c8d47ab1b5
이동욱 <socialable@naver.com>
2022-06-23 13:57:05 +0900

    0623

---
+++ 알고리즘/프로그래머스/2206/0622/README.md
@@ -0,0 +1,5 @@
+# 블로그
+
+### 양궁대회
+
+https://sosohan-pencil-case.tistory.com/18
--- 알고리즘/프로그래머스/2206/0622/프로그래머스_양궁대회.py
+++ 알고리즘/프로그래머스/2206/0622/프로그래머스_양궁대회.py
@@ -1,38 +1,55 @@
+# 완탐(10개만 돌면 되기 때문에)
 def solution(n, info):
+    # 정답
     answer = []
-    num = 0
-    for i in range(len(n)):
-        if info[i]:
-            num += (10 - i)
-    max_num = num + 1
+    # 기본 차이를 1로 만들어야 이길 경우
+    score_gap = 1
 
     # dfs
-    def dfs(last_num, now_info): # 남은 횟수, 현재정보, 현재까지 합
-        nonlocal max_num, answer
+    def dfs(idx, last_num, now_info): # index, 남은 횟수, 현재정보
+        # score_gap과 answer은 바꿔야 하므로 nonlocal로 사용해서 함수 안에서도 변경 할 수 있도록한다.
+        nonlocal score_gap, answer
+        # 맞출 과녁이 0밖에 안남았을 때
+        if idx == 10:
+            # 0에 몰빵해주기
+            now_info[10] += last_num
+            # 어피치와 라이언 스코어 비교
+            lion_num = 0
+            peach_num = 0
+            for j in range(len(now_info) - 1):
+                if now_info[j] > info[j]:
+                    lion_num += 10 - j
+                elif info[j]:
+                    peach_num += 10 - j
 
-        if
-        if not last_num:
-            if now_sum > max_num:
-                max_num = now_sum
-            elif now_sum == max_num:
-                if answer:
-                    now_info.sort()
-                    for j in range(min(len(now_info), len(answer))):
-                        if now_info[j][0] > answer[j][0]:
+            # 이번 갭차이가 더 클때
+            if lion_num - peach_num > score_gap:
+                score_gap = lion_num - peach_num
+                answer = now_info[:]
 
-                else:
+            # 같은 갭일 떄에는 젤 작은 수가 어떤쪽이 많은지 생각 해야한다.
+            elif lion_num - peach_num == score_gap:
+                for j in range(len(now_info)-1, -1, -1):
+                    if now_info[j] > answer[j]:
+                        answer = now_info[:]
+                        break
+                    elif now_info[j] < answer[j]:
+                        break
+            now_info[10] -= last_num
 
-
-    for i in range(len(n)):
-        lst = [0] * 11
-        if info[i]:
-            if info[i] == n:
-                return [-1]
-            else:
-                lst[i] = info[i] + 1
-                dfs(n - 1 - info[i], lst)
         else:
-            lst[i] = 1
-            dfs(n - 1, lst)
+            # 완탐 이번idx에 수를 넣을 경우와 안 넣을 경우
+            if last_num >= info[idx] + 1:
+                lst[idx] = info[idx] + 1
+                dfs(idx + 1, last_num - 1 - info[idx], now_info)
+                lst[idx] = 0
+            dfs(idx + 1, last_num, now_info)
+
+
+    # dfs
+    lst = [0] * 11
+    dfs(0, n, lst)
+    if not answer:
+        answer = [-1]
 
     return answer
